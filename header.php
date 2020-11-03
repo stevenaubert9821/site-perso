01d03995aa5cbb6a4d94edc92859b3a60170d469
Steven Aubert <saubert@sio.local>
2020-11-03 11:00:16 +0100

    first commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php include("data.php"); ?>
+<header class="header_menu">
+  <ul>
+    <li><?php echo $logo[0]; ?></li>
+  </ul>
+  <ul class="list_menu">
+    <li><a href="#"><?php echo $menu[0]; ?></a></li>
+    <li><a href="#"><?php echo $menu[1]; ?></a></li>
+    <li><a href="#"><?php echo $menu[2]; ?> </a></li>
+    <li><a href="#"><?php echo $menu[3]; ?></a></li>
+    <li><a href="#"><?php echo $menu[4]; ?></a></li>
+  </ul>
+</header>
